6a1b7a40de0b6ef3af1f5460b036f43f7e9f6fa5
chyuloveing <chyuloveing@gmail.com>
2026-05-14 13:37:00 +0800

    Update route.ts

--- wedding-wall/app/api/photos/route.ts
+++ wedding-wall/app/api/photos/route.ts
@@ -1,18 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
 import { NextResponse } from "next/server";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export async function GET() {
-  return NextResponse.json([
-    {
-      id: 1,
-      url: "https://picsum.photos/500/700?random=1",
-    },
-    {
-      id: 2,
-      url: "https://picsum.photos/500/700?random=2",
-    },
-    {
-      id: 3,
-      url: "https://picsum.photos/500/700?random=3",
-    },
-  ]);
+
+  try {
+
+    const result = await cloudinary.search
+      .expression("folder:wedding")
+      .sort_by("created_at", "desc")
+      .max_results(100)
+      .execute();
+
+    const photos = result.resources.map((photo: any) => ({
+      url: photo.secure_url,
+      public_id: photo.public_id,
+    }));
+
+    return NextResponse.json(photos);
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json({
+      error: "讀取失敗"
+    });
+
+  }
 }
